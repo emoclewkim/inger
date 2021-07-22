7c287f562b597ac6160890fffbdeaa455bb11536
임완택 <thgod93@naver.com>
2021-07-22 14:23:00 +0900

    User entity + ddl-auto create (User수정시 에러나는 부분 주석처리)

--- backend/src/main/java/com/ssafy/common/auth/SsafyUserDetails.java
+++ backend/src/main/java/com/ssafy/common/auth/SsafyUserDetails.java
@@ -32,11 +32,13 @@ public class SsafyUserDetails implements UserDetails {
     }
 	@Override
 	public String getPassword() {
-		return this.user.getPassword();
+//		return this.user.getPassword();
+		return null;
 	}
 	@Override
 	public String getUsername() {
-		return this.user.getUserId();
+//		return this.user.getUserId();
+		return null;
 	}
 	@Override
 	public boolean isAccountNonExpired() {
