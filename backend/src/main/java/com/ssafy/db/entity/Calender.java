eebaea2d9d411c6c5e616985a01ff1cba5547419
DESKTOP-KVCQHCD\multicampus <llking829@naver.com>
2021-07-22 14:48:26 +0900

    Calender , Todolist PUSH

--- backend/src/main/java/com/ssafy/db/entity/Calender.java
+++ backend/src/main/java/com/ssafy/db/entity/Calender.java
@@ -1,6 +1,10 @@
 package com.ssafy.db.entity;
 
+import java.sql.Date;
+
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -9,5 +13,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Calender extends BaseEntity {
-	
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
+	User user;
+
+	Date date;
+	String promise;
+	String diary;
 }
--- backend/src/main/java/com/ssafy/db/entity/Todolist.java
+++ backend/src/main/java/com/ssafy/db/entity/Todolist.java
@@ -1,6 +1,10 @@
 package com.ssafy.db.entity;
 
+import java.sql.Date;
+
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +12,12 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Todolist extends BaseEntity{
-	
+public class Todolist extends BaseEntity {
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
+	User user;
+
+	Date date;
+	String todo;
+	Boolean is_finish;
 }
