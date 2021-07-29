cb27597a2e3399841142c9a102fd3a883d951386
sungyu_jeon <sungyujeon91@gmail.com>
2021-07-29 21:13:21 +0900

    fix: update frontend

--- frontend/src/components/Entrance/Introduce/styles.jsx
+++ frontend/src/components/Entrance/Introduce/styles.jsx
@@ -1,9 +1,19 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  &.container {
-    padding-left: 0;
-  }
+  background-size: 'contain';
+  width: 100%;
+  height: auto;
+  /* &.background {
+  } */
+  /* .homepage-bgimage {
+    background-image: 'url(./img/inger_background.png)';
+    height: 100vh;
+    width: 100vw;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  } */
 `;
 
 export default Wrapper;
