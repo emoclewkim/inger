62cecac03285cbbf86d719f9b7ac36d74a238551
chaeha <chaeha93@gmail.com>
2021-08-05 19:09:35 +0900

    feat:user

--- backend/src/main/java/com/ssafy/db/entity/UserHistory.java
+++ backend/src/main/java/com/ssafy/db/entity/UserHistory.java
@@ -3,6 +3,7 @@ package com.ssafy.db.entity;
 import java.sql.Date;
 import java.sql.Time;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
