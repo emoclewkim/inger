0c5c1ad7320bbbea73ea4085b87b1be4b8b315a1
DESKTOP-KVCQHCD\multicampus <llking829@naver.com>
2021-08-11 13:11:33 +0900

    [BE] openvidu 내용 삭제

--- backend/src/main/java/com/ssafy/api/controller/ConferenceController.java
+++ backend/src/main/java/com/ssafy/api/controller/ConferenceController.java
@@ -23,16 +23,6 @@ import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
-//import io.openvidu.java.client.*;
-
-//import io.openvidu.java.client.ConnectionProperties;
-//import io.openvidu.java.client.ConnectionType;
-import io.openvidu.java.client.OpenVidu;
-import io.openvidu.java.client.OpenViduHttpException;
-import io.openvidu.java.client.OpenViduJavaClientException;
-import io.openvidu.java.client.OpenViduRole;
-import io.openvidu.java.client.Session;
-
 
 /*
  공부방 관련 요청 처리를 위한 컨트롤러 요청
@@ -44,44 +34,16 @@ public class ConferenceController {
 	@Autowired
 	ConferenceService conferenceService;
 	
-	private OpenVidu openVidu;
-	// URL where our OpenVidu server is listening
-	private String OPENVIDU_URL;
-	// Secret shared with our OpenVidu server
-	private String SECRET;
-	
-	
-	
-	//카테고리를 받아와서
-	
-		// 해당하는 방이 없으면 방을 생성후, 세션값 토큰값 리턴 , DB에 해당 방 저장(noewpeople = 1 , maxpeople = 6 , 날짜시간 현재로, 공부방 종료 날짜 종료시간은 null ) 
-		// 해당하는 방이 있고  maxpeople < 6 이라면, 해당 방의 세션값 토큰값 리턴 , DB에 nowpeople값 ++;
-		// 해당하는 방이 있는데 maxpeople == 6 이라면, 새로운방을 만들어서 ? 	
-	
-	//카테고리를 받아와서 
-		// 해당 카테고리에 대한 
+
 	
 	
 	@GetMapping("/{category}")
 	@ApiOperation(value="해당 카테고리에 맞는 입장버튼 클릭",notes ="")
-	public ResponseEntity<Optional<Conference>> enterConference (@PathVariable Integer category) throws OpenViduJavaClientException, OpenViduHttpException{
+	public ResponseEntity<Optional<Conference>> enterConference (@PathVariable Integer category) {
 		Optional<Conference> conference = conferenceService.getConference(category);
-		// Build connectionProperties object with the serverData and the role
-//		ConnectionProperties connectionProperties = new ConnectionProperties.Builder().type(ConnectionType.WEBRTC)
-//				.role(role).data(serverData).build();
-		
-		if(conference == null) { // 해당카테고리에 맞는 방이 없으면
-			Session session = this.openVidu.createSession();
-			
-//			String token = session.createConnection(connectionProperties).getToken();
+
 			
 			return ResponseEntity.status(200).body(conference);
-			
-		}else { // 해당카테고리에 맞는 방이 있으면
-			
-		}
-		
-		return null;
 	}
 	
 	// 퇴장시
